11a2bda4ae945f2a3a9a72f09fc8610b54f36325
WFeree <ivacyka12@gmail.com>
2025-10-30 17:27:06 +0100

    Student Login new UI

--- frontend/src/components/StudentLogin.tsx
+++ frontend/src/components/StudentLogin.tsx
@@ -1,12 +1,16 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 const StudentLogin = () => {
 //TODO kell userauth meg normalis UI
   return (
-    <div className="mx-auto flex max-w-sm lg:items-center gap-x-4 outline-2 rounded-xl outline-slate-500 flex-col">
-        <h1>Name</h1>
-        <input type="text" className="rounded outline-3 outline-slate-700"/>
-        <h1>Password</h1>
-        <input type="text" className="rounded outline-3 outline-slate-700"/>
-        <button>Enter</button>
+    <div className="my-auto mx-auto flex flex-col p-2 max-w-sm gap-x-4 outline-2 rounded-xl outline-black-500 ">
+        <Label className="py-2">Username</Label>
+        <Input type="text" className=""/>
+        <Label className="py-2">Password</Label>
+        <Input type="text" className=""/>
+        <Button className="mt-2 hover:cursor-pointer">Login</Button>
     </div>
   )
 }
